c6a365367acc7eb66911726dc1dc472f4bff5ccc
Meng Meng <mm90849491@gmail.com>
2016-06-17 21:21:46 +0900

    reroute

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,22 @@ class Userontroller < ApplicationController
        	@user = User.new
        	@user.preference.build
        end
+
+       
+
+  # POST /preferences
+  # POST /preferences.json
+  def create
+    @preference = Preference.new(preference_params)
+
+    respond_to do |format|
+      if @preference.save
+        format.html { redirect_to @preference, notice: 'Preference was successfully created.' }
+        format.json { render :show, status: :created, location: @preference }
+      else
+        format.html { render :new }
+        format.json { render json: @preference.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
