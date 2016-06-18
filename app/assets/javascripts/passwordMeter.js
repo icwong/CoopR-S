c97cdc6e4abdcc08ed29b5457bed7807a33e4625
Justin <jpotusek@live.ca>
2016-06-18 00:20:53 +0000

    merge with usergroups to allow html edit

--- app/assets/javascripts/passwordMeter.js
+++ app/assets/javascripts/passwordMeter.js
@@ -1,3 +1,13 @@
+var password = document.getElementById('password');
+var meter = document.getElementById('passwordMeter');
+
+password.addEventListener('input', function(){
+  meter.value = password.PasswordStrength();
+  
+  
+});
+
+
 function passwordScore(){
     var strength = this.PasswordStrength();
     return strength.score;
