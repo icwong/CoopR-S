f14c5929fd6fecfa7abe1c3c224064f5ec2648cf
Meng Meng <mm90849491@gmail.com>
2016-06-19 12:33:53 +0900

    breaking Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :preferences
+  resources :preferences, :students
   devise_for :users, :controllers => {:registrations => "registrations"}
   
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160617102337) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "type"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
