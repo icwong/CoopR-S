17cf0b9e861a020f73e239fa6d24a9f2714ed44a
Meng Meng <mm90849491@gmail.com>
2016-06-20 16:49:35 +0900

    add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.destroy_all;
-User.create!(email: 'mma140@sfu.ca', :password => 'cors2016', :password_confirmation => 'cors2016', :type => 'Admin');
+root = User.create!(email: 'mma140@sfu.ca', :password => 'cors2016', :password_confirmation => 'cors2016', :type => 'Admin');
+Preference.create!(name: 'Administrator', user_id: root.id);
+comp = User.create!(email: 'company@domain.com', :password => 'cors2016', :password_confirmation => 'cors2016', :type => 'Company');
+Preference.create!(name: 'Mob Company', user_id: comp.id);
+stud = User.create!(email: 'student@domain.com', :password => 'cors2016', :password_confirmation => 'cors2016', :type => 'Student');
+Preference.create!(name: 'Mob Student', user_id: stud.id);
+
 
