cfc630de9012e8e310ae68ecf85bc5a5704ab809
Meng Meng <mm90849491@gmail.com>
2016-07-09 04:05:00 -0700

    refine
    fix one include error, put the login panel to navigation bar, remove
    file with misleading name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
