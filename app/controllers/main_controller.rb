730eb5e5301eb4e3e3396b3f44b515ed6e8c030c
Meng Meng <mm90849491@gmail.com>
2016-07-20 03:43:00 -0700

    change filter logic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,22 @@
 class MainController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    @posts = Post.all.order(id: :asc)
     @uid = params[:id]
+    @type = params[:type]
+    if @uid == nil && @type == nil
+      @posts = Post.all.order(id: :asc)
+    elsif @uid != nil && @type == nil
+      @posts = Post.where("owner = ?", @uid)
+    elsif @uid == nil
+      @posts = Post.where("type = ?", @type)
+    else
+      @posts = Post.where("owner = ? AND type = ?", @uid, @type)
+    end
   end
 
   def filter_by_onwer
     # @posts = Post.all.where("owner = ?", params[:id])
-    @posts = Post.all.order(id: :asc)
-    @uid = params[:id]
+    
   end
 
   def dummy
