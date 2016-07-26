1b0f6339987948e9f8a3dfa04039541c5f69551f
Meng Meng <mm90849491@gmail.com>
2016-07-26 04:29:59 -0700

    done

--- app/controllers/setting_controller.rb
+++ app/controllers/setting_controller.rb
@@ -2,9 +2,18 @@ class SettingController < ApplicationController
   before_action :set_profile, only: [:show]
 
   def display
-    @location = Geocoder.search("V6R1P6")
-    @profile = Profile.find( current_user.id )
-    @preference = Preference.find( current_user.id )
+    @profile = Profile.find_by user_id: current_user.id
+    if @profile == nil
+    	@profile = Profile.new( { "user_id" => current_user.id } )
+    	@profile.save()
+    end
+    @preference = Preference.find_by user_id: current_user.id
+    if @preference != nil
+    	@result = Geocoder.search( @preference.zip )
+    	if @result != nil && @result.first != nil
+    		@location = @result.first.data
+    	end
+	end
   end
 
 end
