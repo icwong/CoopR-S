15cea84aac7ad747f1138cc33fbce46d5ea91304
Ivan <icwong@sfu.ca>
2016-07-26 01:07:13 +0000

    finished, still need to remove pdf link

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -19,5 +19,6 @@ WickedPdf.config = {
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
 # :exe_path => '#{Rails.root}/bin/wkhtmltopdf'
+# :exe_path => '/usr/local/bin/wkhtmltopdf'
   
 }
