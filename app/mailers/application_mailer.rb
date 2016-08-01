bc239432f2905bd6b2cfe401be5122632a8f8c1d
Ivan <icwong@sfu.ca>
2016-08-01 05:37:48 +0000

    asdasd

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: "application@cooprs.herokuapp.com"
-  default from: "application@salty-garden-19115.herokuapp.com
+  default from: "application@salty-garden-19115.herokuapp.com" 
   layout 'mailer'
 end
