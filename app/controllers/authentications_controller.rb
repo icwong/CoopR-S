64ef60ad373f447167d098c72eda91caaba49fa1
Meng Meng <mm90849491@gmail.com>
2016-08-01 20:31:28 +0900

    restriction

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -15,6 +15,14 @@ class AuthenticationsController < ApplicationController
 		@me = current_user
 		@sid = hash( @me.id )
     @auth = Authentication.find_by user_id: current_user.id
+
+    if !@user.email.include? "@ubc.ca" && 
+       !@user.email.include? "@sfu.ca" && 
+       !@user.email.include? "@myLangara.bc.ca"
+      redirect_to fail_path
+      flash[:notice] = 'Your email is not issued by a BC university/college.' 
+    end
+
     if @auth.nil?
       @auth = Authentication.new( {:user_id => @me.id, :serial => @sid} )
       @auth.save
